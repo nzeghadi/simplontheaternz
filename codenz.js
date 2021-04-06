b86c7a66331f621b6d73dc86fc0f687ff9b98edc
nzeghadi <82052226+nzeghadi@users.noreply.github.com>
2021-04-06 23:00:32 +0200

    Update and rename codenz to codenz.js

---
+++ codenz.js
@@ -0,0 +1,16 @@
+// construction du tableau
+
+var tab=new Array();
+
+// incrémentation lignes colonnes
+tab[0]=1;
+tab.length=8;
+
+//affichage menu
+<form name="f1"> "Combien de places voulez-vous ?"
+<input type="text" name="nbplace" size=5 value=0>
+"Quelle rangée ?"
+<input type="text" name="rangee" size=5 value=0>
+</form>
+// affichage tableau
+
